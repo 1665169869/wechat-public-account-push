55012a3b223713a7f91d17481d2f1960d8e24b19
白羽 <50762038+1665169869@users.noreply.github.com>
2022-10-08 13:19:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: 'Blue', year: '2005', date: '01-21',
+          type: '*生日', name: 'Blue农历', year: '2005', date: '01-21',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2005', date: '03-01',
+          type: '生日', name: 'Blue', year: '2005', date: '03-01',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '09-04',
